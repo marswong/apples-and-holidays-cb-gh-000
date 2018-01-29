ff14542f13329bfad35a4ad00c73810c32f7df18
Mars Wong <marswong618@gmail.com>
2018-01-29 07:06:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,12 +62,12 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{sesson}:"
     holidays.each do |holiday, supplies|
       puts "#{holiday}: #{supplies.join(", ")}"      
-    end    
+    end
   end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.values.flatten.select { |k, v| v.include?("BBQ") }
 end
