f5f438f955e58d9dc48dc15e3f7ebee69ad998ad
Mars Wong <marswong618@gmail.com>
2018-01-29 07:34:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       result << holiday.to_s if supplies.include?("BBQ")      
-    end    
+    end
   end
   result
 end
